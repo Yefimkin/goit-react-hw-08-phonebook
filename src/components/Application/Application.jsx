b975db9a6f7b07681cd7ce3ed94f415ddff738b1
Yefimkin Denys <iefimkindenis@gmail.com>
2022-11-11 19:25:36 +0200

    update

--- src/components/Application/Application.jsx
+++ src/components/Application/Application.jsx
@@ -1,18 +1,18 @@
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
-import Container from './Container/Container';
+import style from './Application.module.css';
 
 const Application = () => {
   return (
-    <Container>
-      <h1>Phonebook</h1>
+    <>
+      <h1 className={style.title}>Phonebook</h1>
       <ContactForm />
 
-      <h2>Contacts</h2>
+      <h2 className={style.title}>Contacts</h2>
       <Filter />
       <ContactList />
-    </Container>
+    </>
   );
 };
 
--- src/components/Application/ContactForm/ContactForm.js
+++ src/components/Application/ContactForm/ContactForm.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getContacts } from 'redux/contacts/contactsSlice';
 import { addContact } from 'redux/contacts/contactsOperation';
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 
 import { Loader } from '../Loader/Loader';
 import { toast } from 'react-toastify';
@@ -10,81 +10,96 @@ import { toast } from 'react-toastify';
 import style from './ContactForm.module.css';
 
 const ContactForm = () => {
-    const [name, setName] = useState('');
-    const [number, setNumber] = useState('');
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        
-        switch (name) {
-            case 'name':
-                setName(value)
-                break;
-            case 'number':
-                setNumber(value)
-                break;
-          default:
-        }
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+    }
+  };
+
+  const dispatch = useDispatch();
+  const { items, addingLoader } = useSelector(getContacts);
+
+  const contactAlreadyExists = (name, number) => {
+    return items.find(
+      item =>
+        item.name.toLocaleLowerCase() === name.toLocaleLowerCase() ||
+        item.number === number
+    );
+  };
+
+  const addContactToList = (id, name, number) => {
+    if (contactAlreadyExists(name, number)) {
+      return toast.error(`${name} ${number} is already in Phonebook`);
     }
 
-    const dispatch = useDispatch();
-    const {items, addingLoader} = useSelector(getContacts);
+    dispatch(addContact({ id, name, number }));
+    setName('');
+    setNumber('');
+  };
 
-    const contactAlreadyExists = (name, number) => {
-        return items.find((item) => item.name.toLocaleLowerCase() === name.toLocaleLowerCase() || item.number === number);
-    }
+  const onFormSubmit = e => {
+    e.preventDefault();
 
-    const addContactToList = (id, name, number) => {
-        if (contactAlreadyExists(name, number)) {
-            return toast.error(`${name} ${number} is already in Phonebook`)
-        }
+    addContactToList(nanoid(), name, number);
+  };
 
-      dispatch(addContact({ id, name, number }));
-      setName('')
-      setNumber('')
-    }
-
-    const onFormSubmit = (e) => {
-        e.preventDefault();
-
-        addContactToList(nanoid(), name, number);
-    }
-
-    const nameId = nanoid();
-    const numberId = nanoid();
+  const nameId = nanoid();
+  const numberId = nanoid();
 
   return (
     <form onSubmit={onFormSubmit}>
-      <label className={style.form} htmlFor={nameId}>Name</label>
-        <input
-          id={nameId}
-          onChange={handleInputChange}
-          value={name}
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          className={style.input}
-        />
-        <label htmlFor={numberId}>Number</label>
-        <input
-          id={numberId}
-          onChange={handleInputChange}
-          value={number}
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          className={style.input}
+      <label className={style.form} htmlFor={nameId}>
+        Name
+      </label>
+      <input
+        id={nameId}
+        onChange={handleInputChange}
+        value={name}
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        className={style.input}
       />
-      {addingLoader ?
-        <Loader /> :
-        <button type='submit' className={style.addButton} disabled={number && name ? false : true}>Add contact</button>
-      }
+      <label className={style.form} htmlFor={numberId}>
+        Number
+      </label>
+      <input
+        id={numberId}
+        onChange={handleInputChange}
+        value={number}
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        className={style.input}
+      />
+      {addingLoader ? (
+        <Loader />
+      ) : (
+        <button
+          type="submit"
+          className={style.addButton}
+          disabled={number && name ? false : true}
+        >
+          Add contact
+        </button>
+      )}
     </form>
   );
-}
+};
 
 export default ContactForm;
